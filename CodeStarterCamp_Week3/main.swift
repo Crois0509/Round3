a90d16af574b52f29a7975213f8a41854861064e
Crois0509 <crois0509@naver.com>
2024-07-24 21:50:55 +0900

    [Round3/Step1]-Crois_커피 주문하기

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,12 +1,12 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week3
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+var misterLee: Person = Person(name: "mrLee", age: 32, gender: "male", money: 2500)
+var missKim: Person = Person(name: "msKim", age: 25, gender: "female", money: 10000)
+var yagombucks: CoffeeShop = CoffeeShop(name: "yagombucks", barista: misterLee.name, sales: 0)
 
+for coffee in yagombucks.menu {
+    missKim.buyCoffee(from: yagombucks, to: coffee)
+    print(" ")
+}
+
+todaysCalculate(yagombucks)
